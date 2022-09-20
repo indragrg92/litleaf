0de11e28f52732ecc1f06efbbf1870b2e757c207
Indra <indragrg.dev@gmail>
2022-09-20 18:15:30 +0545

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+
+});
+
+jQuery( document ).on( 'click', '.smooth-scroll', function( e ) {
+
+   if (this.hash !== "") {
+         // Prevent default anchor click behavior
+         e.preventDefault();
+
+         // Store hash
+         var hash = this.hash;
+
+         $('html, body').animate({
+            scrollTop: $(hash).offset().top
+         }, 800, function(){
+   
+        window.location.hash = hash;
+      });
+    } 
+} );
