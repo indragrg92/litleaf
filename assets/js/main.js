8a33d401c350d6e8e1fc4ad8aed8a2ce359806e8
Indra <indragrg.dev@gmail>
2022-10-10 17:32:21 +0545

    small changes added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,63 @@
-jQuery(document).ready(function() {
+jQuery(document).ready(function () {
 
-   jQuery( '.navbar' ).on( 'click' , '.hamburger-menu', function (e) {
+   jQuery('.navbar').on('click', '.hamburger-menu', function (e) {
 
-       e.preventDefault();
+      e.preventDefault();
 
-       if ( !jQuery( this ).hasClass( 'nav-open' ) ) {
+      if (!jQuery(this).hasClass('nav-open')) {
 
-           jQuery( '#navbarSupportedContent' ).removeClass( 'active' );
-           jQuery( 'body' ).removeClass('menu-active');
-           jQuery( this ).removeClass('nav-close').addClass('nav-open');
+         jQuery('#navbarSupportedContent').removeClass('active');
+         jQuery('body').removeClass('menu-active');
+         jQuery(this).removeClass('nav-close').addClass('nav-open');
 
-       } else {
+      } else {
 
-           jQuery( '#navbarSupportedContent' ).addClass( 'active' );
-           jQuery( 'body' ).addClass( 'menu-active' );
-           jQuery( this ).removeClass( 'nav-open' ).addClass( 'nav-close' );
+         jQuery('#navbarSupportedContent').addClass('active');
+         jQuery('body').addClass('menu-active');
+         jQuery(this).removeClass('nav-open').addClass('nav-close');
 
-       }
+      }
    });
 
-   jQuery( '.submenu-opener' ).on( 'click', function( e ) {
-       e.preventDefault();
+   jQuery('.submenu-opener').on('click', function (e) {
+      e.preventDefault();
 
-       if( !jQuery( this ).hasClass( 'closed' ) ) {
-           jQuery( this ).addClass( 'closed' );
-           jQuery( this ).next( 'ul.dropdown-menu' ).css( 'display', 'block' );
-       }
-       else {
-           jQuery( this ).removeClass( 'closed' );
-           jQuery( this ).next( 'ul.dropdown-menu' ).css( 'display', 'none' );   
-       }
-   } );
+      if (!jQuery(this).hasClass('closed')) {
+         jQuery(this).addClass('closed');
+         jQuery(this).next('ul.dropdown-menu').css('display', 'block');
+      }
+      else {
+         jQuery(this).removeClass('closed');
+         jQuery(this).next('ul.dropdown-menu').css('display', 'none');
+      }
+   });
 
 
 });
 
-jQuery( document ).on( 'click', '.smooth-scroll', function( e ) {
+jQuery(document).on('click', '.smooth-scroll', function (e) {
 
    if (this.hash !== "") {
-         // Prevent default anchor click behavior
+      // Prevent default anchor click behavior
       e.preventDefault();
 
-      jQuery( '.hamburger-menu' ).removeClass('nav-close').addClass('nav-open');
-      jQuery( '#navbarSupportedContent' ).removeClass( 'active' );
-      jQuery( 'body' ).removeClass('menu-active');
-      
+      jQuery('.hamburger-menu').removeClass('nav-close').addClass('nav-open');
+      jQuery('#navbarSupportedContent').removeClass('active');
+      jQuery('body').removeClass('menu-active');
 
-         // Store hash
-         var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-         $('html, body').animate({
-            scrollTop: $(hash).offset().top
-         }, 800, function(){
-   
-        window.location.hash = hash;
+      $('html, body').animate({
+         scrollTop: $(hash).offset().top - 500
+      }, 800, function () {
+         window.location.hash = hash;
       });
-    } 
-} );
+   }
+});
 
-jQuery(document).on( 'submit', '#joinbox-form', function(e) {
-    location.href = 'https://www.premint.xyz/litleaf';
+jQuery(document).on('submit', '#joinbox-form', function (e) {
+   location.href = 'https://www.premint.xyz/litleaf';
 
-    return false;
-} );
+   return false;
+});
